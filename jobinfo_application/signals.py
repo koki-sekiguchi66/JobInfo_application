c0834adfffb283480372b301fc79488f4f702892
koki-sekiguchi66 <sekiguchi.kouki.34k@st.kyoto-u.ac.jp>
2025-06-30 17:30:00 +0900

    update user profile

--- jobinfo_application/signals.py
+++ jobinfo_application/signals.py
@@ -5,11 +5,9 @@ from .models import UserProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    """Userが作成されたら、自動的にUserProfileも作成する"""
     if created:
         UserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    """Userが保存されたら、UserProfileも保存する"""
     instance.profile.save()
