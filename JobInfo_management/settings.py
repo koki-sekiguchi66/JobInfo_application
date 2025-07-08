be3d98bbc56e856683a7303c46fefdba095bede8
koki-sekiguchi66 <sekiguchi.kouki.34k@st.kyoto-u.ac.jp>
2025-07-08 20:22:09 +0900

    finish deploy

--- JobInfo_management/settings.py
+++ JobInfo_management/settings.py
@@ -131,7 +131,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'application-list'
 LOGOUT_REDIRECT_URL = 'application-list'
-# パスワードリセット用のメール設定（開発用なのでコンソールに出力）
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# パスワードリセット用のメール設定
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = env('EMAIL_HOST')
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = env.int('EMAIL_PORT', default=587)
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = f"no-reply@{env('RENDER_EXTERNAL_HOSTNAME', default='yourdomain.com')}"
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 OPENAI_API_KEY = env('OPENAI_API_KEY')
