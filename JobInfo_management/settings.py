803f43f12f045277a9c9328d338b7529e086a0da
koki-sekiguchi66 <sekiguchi.kouki.34k@st.kyoto-u.ac.jp>
2025-07-08 19:11:28 +0900

    finish deploy

--- JobInfo_management/settings.py
+++ JobInfo_management/settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+
 
 
 # Application definition
@@ -31,11 +32,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobinfo_application.apps.JobinfoApplicationConfig'
+    'jobinfo_application.apps.JobinfoApplicationConfig',
+    'whitenoise.runserver_nostatic',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -69,8 +74,11 @@ WSGI_APPLICATION = 'JobInfo_management.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db()
+    'default': dj_database_url.config(
+        default=env('DATABASE_URL')
+    )
 }
+
 
 
 # Password validation
@@ -108,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
