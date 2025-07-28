9e3e3b935c12988463032194dea555681c90cc92
koki-sekiguchi66 <sekiguchi.kouki.34k@st.kyoto-u.ac.jp>
2025-07-28 09:32:44 +0900

    activation

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+
 pip install -r requirements.txt
 
+
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+
+python manage.py migrate --no-input
+
