c462e907e6c93919305acb8bd5d1593de8a30bdf
koki-sekiguchi66 <sekiguchi.kouki.34k@st.kyoto-u.ac.jp>
2025-07-28 09:11:17 +0900

    activation

--- JobInfo_management/settings.py
+++ JobInfo_management/settings.py
@@ -111,16 +111,7 @@ LOGOUT_REDIRECT_URL = 'application-list'
 
 # パスワードリセット用のメール設定
 
-if DEBUG is False:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = env('EMAIL_HOST')
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    EMAIL_PORT = env.int('EMAIL_PORT', default=587)
-    EMAIL_USE_TLS = True 
-    DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 
