ea373bec4ac4eb9f2a0b68db3f381c650caac52a
koki-sekiguchi66 <sekiguchi.kouki.34k@st.kyoto-u.ac.jp>
2025-07-28 14:20:56 +0900

    activation

--- jobinfo_application/views.py
+++ jobinfo_application/views.py
@@ -1,15 +1,14 @@
 import requests
-import xml.etree.ElementTree as ET
+
 from openai import OpenAI
 from django.http import JsonResponse
 from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, get_user_model
+from django.contrib.auth import login
 from django.contrib import messages
-from django.utils import timezone
 from .models import (
-    JobApplication, Document, UserProfile, JobType,
+    JobApplication, UserProfile, JobType,
     InterviewLog, EntrySheet
 )
 from .forms import (
